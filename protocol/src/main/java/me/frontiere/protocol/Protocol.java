b4d9d8fc634d42e9b386228a631bceb318584e20
Beeteee <beeteeedev@gmail.com>
2016-10-18 15:06:24 +0200

    moving packet class and adding logic to the Protocol enum

--- protocol/src/main/java/me/frontiere/protocol/Protocol.java
+++ protocol/src/main/java/me/frontiere/protocol/Protocol.java
@@ -1,11 +1,33 @@
 package me.frontiere.protocol;
 
+import lombok.Getter;
+import me.frontiere.protocol.packet.Packet;
+
 /**
  * Created by Overwatch on 18.10.2016.
  */
+@Getter
 public enum Protocol {
     ;
-
     private int packetId;
+    private Class<? extends Packet> packetClass;
 
+    Protocol(int packetId, Class<? extends Packet> packetClass) {
+        this.packetId = packetId;
+        this.packetClass = packetClass;
+    }
+
+    public static Class<? extends Packet> getPacketFromId(int packetId) {
+        for(Protocol protocol : values())
+            if(protocol.getPacketId() == packetId)
+                return protocol.getPacketClass();
+        return null;
+    }
+
+    public static int getIdFromPacket(Class<? extends Packet> packetClass) {
+        for(Protocol protocol : values())
+            if(protocol.getPacketClass() == packetClass)
+                return protocol.getPacketId();
+        return -1;
+    }
 }
---
+++ protocol/src/main/java/me/frontiere/protocol/packet/Packet.java
@@ -0,0 +1,18 @@
+package me.frontiere.protocol.packet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Class Created by Traape | Jan.
+ * 18.10.2016
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Packet {
+    public abstract void read( ByteBufInputStream byteBuf );
+    public abstract void write( ByteBufOutputStream byteBuf );
+}
