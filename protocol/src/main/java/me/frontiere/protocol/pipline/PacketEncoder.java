21a796a8816d2126d9fd5e67c16075783f4fb19e
Beeteee <beeteeedev@gmail.com>
2016-10-18 15:14:49 +0200

    Adding PacketEncoder

---
+++ protocol/src/main/java/me/frontiere/protocol/pipline/PacketEncoder.java
@@ -0,0 +1,23 @@
+package me.frontiere.protocol.pipline;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import me.frontiere.protocol.Protocol;
+import me.frontiere.protocol.packet.Packet;
+
+/**
+ * Created by Overwatch on 18.10.2016.
+ */
+public class PacketEncoder extends MessageToByteEncoder<Packet> {
+    protected void encode(ChannelHandlerContext channelHandlerContext,
+                          Packet packet,
+                          ByteBuf byteBuf) throws Exception {
+        int packetId = Protocol.getIdFromPacket(packet.getClass());
+        if(packetId > -1) {
+            byteBuf.writeInt(packetId);
+            packet.write(new ByteBufOutputStream(byteBuf));
+        }
+    }
+}
